ffc0abd00e723e06cd8bf8d08fe1716a997b43d9
happy6307 <aaibhav6307@gmail.com>
2025-03-27 12:20:12 +0530

    modified:   form.js 	modified:   node.js 	modified:   tt.css

--- form.js
+++ form.js
@@ -18,3 +18,28 @@ function calculateAge()
         document.getElementById('age').value=' ';
     }
 }
+const express=require ('express');
+const mongoose=require ('mongoose');
+const path=require('path');
+const bodyparser=require('bodyparser');
+const formrouter=require('./routes/formRoutes');
+const bodyParser = require('body-parser');
+const { json } = require('stream/consumers');
+
+const app=express();
+const PORT = 3000;
+
+mongoose.connect(' mongodb://localhost:27017/formDB',{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+});
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.use(express.static('public'));
+app.use('/api/form', formRoutes);
+
+app.listen(PORT,()=>{
+    console.log(`Server running at http://localhost:${PORT}`);
+});
